ace7cd981220d0cf6b81777395397208eedd124b
Adam Therrien-Marois <adamletannant@hotmail.ca>
2023-09-25 12:22:17 -0400

    start remade

---
+++ APIDynamic/APIDynamic/ParamLinker.cs
@@ -0,0 +1,9 @@
+﻿namespace APIDynamic
+{
+    public class ParamLinker
+    {
+        public string AssociatedVarName { get; internal set; }
+        public string Value { get; internal set; }
+        public CSharpTypes CSharpType { get; internal set; }
+    }
+}
