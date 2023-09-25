1d6baef7412b8a8468e369a6475c38407f60f49b
Antoine <antoinecegep201946231@gmail.com>
2023-09-25 10:18:13 -0400

    Revert changes
    Revert Changes

--- APIDynamic/APIDynamic/DynamicQueryForRoute.cs
+++ APIDynamic/APIDynamic/DynamicQueryForRoute.cs
@@ -9,24 +9,6 @@ namespace APIDynamic
     {
         public long id { get; set; }
         public Query query { get; set; }
-<<<<<<< Updated upstream
-        public List<DynamicFilter> filters { get; set; }
-        public static readonly Query getFilters = Query.fromQueryString(QueryTypes.SELECT, "SELECT Filters.id AS id, Filters.name AS Name, varAffected AS VarAffected, ind AS [Index], id_ShowType AS IDShowType, ShowTypes.name AS ShowTypeName FROM Filters INNER JOIN ShowTypes ON ShowTypes.id = id_ShowType WHERE id_RouteQuery = @routeQueryID ORDER BY Filters.name, ind");
-        public static readonly Query insertRouteQuery = Query.fromQueryString(QueryTypes.INSERT, "INSERT INTO RouteQueries (ind, SQLString, id_queryType, id_route, completeCheck) SELECT MAX(ind) + 1, @SQLString, @QueryTypeID, @RouteID, @CompleteCheck FROM RouteQueries WHERE id_route = @RouteID");
-        internal DynamicQueryForRoute(long id, string queryString, long IDQueryType, bool CompleteCheck)
-        {
-            this.id = id;
-            this.query = Query.fromQueryString((QueryTypes)IDQueryType, queryString, CompleteCheck);
-        }
-        internal static async Task<DynamicQueryForRoute> init(DynamicQueryForRoute query)
-        {
-            query.filters = (await DynamicController.executor.SelectQueryTotal<DynamicFilter>(getFilters.setParam("routeQueryID", query.id))).ToList();
-            return query;
-        }
-        public async static Task addRouteQuery(List<DynamicQueryForRoute> routeQueries, string queryString, QueryTypes IDQueryType, long RouteID, bool CompleteCheck)
-        {
-            routeQueries.Add(new DynamicQueryForRoute(await DynamicController.executor.ExecuteInsertWithLastID(insertRouteQuery.setParam("SQLString", queryString).setParam("QueryTypeID", (long)IDQueryType).setParam("RouteID", RouteID).setParam("CompleteCheck", CompleteCheck)), queryString, (long)IDQueryType, CompleteCheck));
-=======
         public bool CompleteAuth { get; set; }
         public Dictionary<string, DynamicSQLParamInfo> ParamsInfos { get; set; }
         public List<DynamicFilter> filters { get; set; }
@@ -73,7 +55,6 @@ namespace APIDynamic
             }
 
             return dynamicQueryForRoute;
->>>>>>> Stashed changes
         }
         public Task addFilters(string name, long ShowTypeID, string VarAffected)
         {
