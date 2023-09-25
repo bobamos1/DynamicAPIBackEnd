83f4bc289768029cea4886ede263650d84f7e7ca
Antoine <antoinecegep201946231@gmail.com>
2023-09-25 19:52:41 -0400

    Update BDInit.cs
    clients et commandes

--- APIDynamic/APIDynamic/BDInit.cs
+++ APIDynamic/APIDynamic/BDInit.cs
@@ -64,7 +64,7 @@ namespace APIDynamic
                             .addSQLParamInfo("id_ville", null)
                             .addSQLParamInfo("id_employe", null)
                     .addRoute("Commandes", BaseRoute.UPDATE)
-                        .addRouteQuery("Commandes", BaseRoutes.UPDATE.Value(), "UPDATE commandes SET numero_facture = @_numero_facture, montant_brut = @_montant_brut, no_civique_livraison = @_no_civique_livraison, rue_livraison = @_rue_livraison, id_client = @_id_client, id_etat_commande = @_id_etat_commande, id_ville = @_id_ville, id_employe = @_id_employe WHERE id = @id)", QueryTypes.UPDATE, true, false)
+                        .addRouteQuery("Commandes", BaseRoutes.UPDATE.Value(), "UPDATE commandes SET numero_facture = @_numero_facture, montant_brut = @_montant_brut, no_civique_livraison = @_no_civique_livraison, rue_livraison = @_rue_livraison, id_client = @_id_client, id_etat_commande = @_id_etat_commande, id_ville = @_id_ville, id_employe = @_id_employe WHERE id = @id", QueryTypes.UPDATE, true, false)
                             .addSQLParamInfo("id", null)
                             .addSQLParamInfo("numero_facture", null)
                             .addSQLParamInfo("montant_brut", null)
@@ -74,8 +74,58 @@ namespace APIDynamic
                             .addSQLParamInfo("id_etat_commande", null)
                             .addSQLParamInfo("id_ville", null)
                             .addSQLParamInfo("id_employe", null)
+                    .addRoute("Commandes", "GetClientsCommande")
+                        .addRouteQuery("Commandes", BaseRoutes.UPDATE.Value(), "UPDATE commandes SET numero_facture = @_numero_facture, montant_brut = @_montant_brut, no_civique_livraison = @_no_civique_livraison, rue_livraison = @_rue_livraison, id_client = @_id_client, id_etat_commande = @_id_etat_commande, id_ville = @_id_ville, id_employe = @_id_employe WHERE id_client = @id_client", QueryTypes.UPDATE, true, false)
+                            .addSQLParamInfo("numero_facture", null)
+                            .addSQLParamInfo("montant_brut", null)
+                            .addSQLParamInfo("no_civique_livraison", null)
+                            .addSQLParamInfo("rue_livraison", null)
+                            .addSQLParamInfo("id_client", null)
+                            .addSQLParamInfo("id_etat_commande", null)
+                            .addSQLParamInfo("id_ville", null)
+                            .addSQLParamInfo("id_employe", null)
+
+                .addController("Clients", true)
+                    .addRoute("Clients", BaseRoute)
+                        .addRouteQuery("Clients", BaseRoutes.GETALL.Value(), "SELECT id, nom, prenom, date_naissance, adresse_courriel, mdp, token, sel, actif FROM clients", QueryTypes.SELECT, true, false)
+                    .addRoute("Clients", BaseRoutes.GET)
+                        .addRouteQuery("Clients", BaseRoutes.GET.Value(), "SELECT id, nom, prenom, date_naissance, adresse_courriel, mdp, token, sel, actif FROM clients WHERE p.id = @id", QueryTypes.SELECT, true, false)
+                            .addSQLParamInfo("id", null)
+                    .addRoute("Clients", BaseRoutes.INSERT)
+                        .addRouteQuery("Clients", BaseRoutes.INSERT.Value(), "INSERT INTO clients (id, nom, prenom, date_naissance, adresse_courriel, mdp, token, sel, actif) VALUES (@id, @nom, @prenom, @date_naissance, @adresse_courriel, @mdp, @token, @sel, @actif)", QueryTypes.INSERT, true, false)
+                            .addSQLParamInfo("nom", null)
+                            .addSQLParamInfo("prenom", null)
+                            .addSQLParamInfo("date_naissance", null)
+                            .addSQLParamInfo("adresse_courriel", null)
+                            .addSQLParamInfo("mdp", null)
+                            .addSQLParamInfo("token", null)
+                            .addSQLParamInfo("sel", null)
+                            .addSQLParamInfo("actif", null)
+                    .addRoute("Clients", BaseRoutes.UPDATE)
+                        .addRouteQuery("Clients", BaseRoutes.UPDATE.Value(), "UPDATE clients SET nom = @_nom, prenom = @_prenom, date_naissance = @_date_naissance, adresse_courriel = @_adresse_courriel, mdp = @_mdp, token = @_token, sel = @_sel, actif = @_actif WHERE id = @id", QueryTypes.UPDATE, true, false)
+                            .addSQLParamInfo("nom", null)
+                            .addSQLParamInfo("prenom", null)
+                            .addSQLParamInfo("date_naissance", null)
+                            .addSQLParamInfo("adresse_courriel", null)
+                            .addSQLParamInfo("mdp", null)
+                            .addSQLParamInfo("token", null)
+                            .addSQLParamInfo("sel", null)
+                            .addSQLParamInfo("actif", null)
 
                 ;
+
+
+
+
+
+                      ,[nom]
+      ,[prenom]
+      ,[date_naissance]
+      ,[adresse_courriel]
+      ,[mdp]
+      ,[token]
+      ,[sel]
+      ,[actif]
         }
     }
 }
