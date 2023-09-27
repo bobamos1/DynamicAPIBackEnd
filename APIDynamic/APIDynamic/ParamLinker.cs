cbd1a4d3fe3c4dcf766eb73a3fcbedd8042cfd21
Adam Therrien-Marois <adamletannant@hotmail.ca>
2023-09-26 20:46:48 -0400

    up to date

--- APIDynamic/APIDynamic/ParamLinker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIDynamic
-{
-    public class ParamLinker
-    {
-        public string AssociatedVarName { get; internal set; }
-        public string Value { get; internal set; }
-        public CSharpTypes CSharpType { get; internal set; }
-    }
-}
--- APIDynamic/APIDynamic/ValidatorTypes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace APIDynamic
-{
-    public class ValidatorTypes
-    {
-    }
-}
