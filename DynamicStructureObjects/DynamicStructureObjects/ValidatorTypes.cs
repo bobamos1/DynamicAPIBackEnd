fccde08336347fa46bac39155ac2bb8bf5e3eb35
Antoine Plouffe <119693853+AntoPlouffee@users.noreply.github.com>
2023-09-29 14:05:11 -0400

    lib
    Librairie sans espace dans le using namespace

--- DynamicStructureObjects/DynamicStructureObjects/ValidatorTypes.cs
+++ DynamicStructureObjects/DynamicStructureObjects/ValidatorTypes.cs
@@ -3,6 +3,10 @@
     public enum ValidatorTypes
     {
         [Value("Required")]
-        REQUIRED = 1
+        REQUIRED = 1,
+        [Value("Max")]
+        MAX = 2,
+        [Value("Min")]
+        MIN = 3
     }
 }
---
+++ sendEmailLib/send email/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Collections.Generic;
+
+namespace sendEmail
+{
+    public class EmailSender
+    {
+
+        private string _from;
+        private string _smtpUsername;
+        private string _smtpPassword;
+        private int _host = 587;
+        private Dictionary<string, string> bodiesSubjects = new Dictionary<string, string>();
+
+        public string from
+        {
+            get { return _from; }
+            set { _from = value; }
+        }
+
+        public string smtpUsername
+        {
+            get { return _smtpUsername; }
+            set { _smtpUsername = value; }
+        }
+
+        public string smtpPassword
+        {
+            get { return _smtpPassword; }
+            set { _smtpPassword = value; }
+        }
+        public int host
+        {
+            get { return _host; }
+            set { _host = value; }
+        }
+
+        public EmailSender(string fromEmail, string smtpUsername, string smtpPassword)
+        {
+            _from = fromEmail;
+            _smtpUsername = smtpUsername;
+            _smtpPassword = smtpPassword;
+        }
+        public static void SendEmail(string fromEmail, List<string> toEmails, string subject, string body, string smtpUsername, string smtpPassword)
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(fromEmail);
+
+            foreach (var toEmail in toEmails)
+            {
+                mailMessage.To.Add(toEmail);
+            }
+
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+            smtpClient.EnableSsl = true;
+
+            try
+            {
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Email Sent Successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public void SendEmail(List<string> toEmails, string subject, string body)
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(_from);
+
+            foreach (var toEmail in toEmails)
+            {
+                mailMessage.To.Add(toEmail);
+            }
+
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(_smtpUsername, _smtpPassword);
+            smtpClient.EnableSsl = true;
+
+            try
+            {
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Email Sent Successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
